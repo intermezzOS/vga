f071a0f1de4ac433736130d50d2f069f7a60e980
Steve Klabnik <steve@steveklabnik.com>
2016-09-24 16:15:05 -0400

    Multiple writes produce the correct output
    Before, each write would start at the beginning of the buffer, but we
    actually want it to be stateful. This adds the proper state to make it
    work for the second call.

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,7 @@ use core::fmt::Write;
 pub struct Vga {
     location: *mut u8,
     buffer: [u8; 25 * 80 * 2],
+    position: usize,
 }
 
 impl Vga {
@@ -13,6 +14,7 @@ impl Vga {
         Vga {
             location: location,
             buffer: [0; 25 * 80 * 2],
+            position: 0,
         }
     }
 
@@ -29,11 +31,15 @@ impl Vga {
 
 impl Write for Vga {
     fn write_str(&mut self, s: &str) -> Result<(), fmt::Error> {
-        for (i, b) in s.bytes().enumerate() {
-            let i = i * 2;
+        for b in s.bytes() {
+            let i = self.position;
+
             self.buffer[i] = b;
             self.buffer[i + 1] = 0x02;
+
+            self.position += 2;
         }
+
         Ok(())
     }
 }
@@ -71,5 +77,37 @@ mod tests {
         assert_eq!(vga.buffer[5], 0x02);
         assert_eq!(vga.buffer[6], 'd' as u8);
         assert_eq!(vga.buffer[7], 0x02);
+    }
+
+    #[test]
+    fn write_multiple_words() {
+        let mut mock_memory = [0u8; 25 * 80];
+        let mut vga = unsafe { Vga::new(mock_memory.as_mut_ptr()) };
+
+        vga.write_str("hello ").unwrap();
+        vga.write_str("world").unwrap();
+      
+        assert_eq!(vga.buffer[0], 'h' as u8);
+        assert_eq!(vga.buffer[1], 0x02);
+        assert_eq!(vga.buffer[2], 'e' as u8);
+        assert_eq!(vga.buffer[3], 0x02);
+        assert_eq!(vga.buffer[4], 'l' as u8);
+        assert_eq!(vga.buffer[5], 0x02);
+        assert_eq!(vga.buffer[6], 'l' as u8);
+        assert_eq!(vga.buffer[7], 0x02);
+        assert_eq!(vga.buffer[8], 'o' as u8);
+        assert_eq!(vga.buffer[9], 0x02);
+        assert_eq!(vga.buffer[10], ' ' as u8);
+        assert_eq!(vga.buffer[11], 0x02);
+        assert_eq!(vga.buffer[12], 'w' as u8);
+        assert_eq!(vga.buffer[13], 0x02);
+        assert_eq!(vga.buffer[14], 'o' as u8);
+        assert_eq!(vga.buffer[15], 0x02);
+        assert_eq!(vga.buffer[16], 'r' as u8);
+        assert_eq!(vga.buffer[17], 0x02);
+        assert_eq!(vga.buffer[18], 'l' as u8);
+        assert_eq!(vga.buffer[19], 0x02);
+        assert_eq!(vga.buffer[20], 'd' as u8);
+        assert_eq!(vga.buffer[21], 0x02);
     }
 }
